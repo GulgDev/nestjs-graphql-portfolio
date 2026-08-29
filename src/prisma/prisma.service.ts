226cee60cbb3ec121a3629d80da10468d4e3346d
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-08-30 03:08:30 +0500

    Switch to LibSql (for Turso)

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, PrismaClient } from '../generated/prisma/client.js';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaLibSql } from '@prisma/adapter-libsql';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
     super({
-      adapter: new PrismaBetterSqlite3({ url: process.env.DATABASE_URL }),
+      adapter: new PrismaLibSql({
+        url: process.env.DATABASE_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN,
+      }),
     });
   }
 }
